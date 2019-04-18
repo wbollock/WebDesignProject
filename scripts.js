876cdb1df67ed8c4ffd9c9f60408461902057eac
Isaac Wolfe <iwwolfe97@gmail.com>
2019-04-18 09:08:20 -0400

    Added newuser form validation

--- scripts.js
+++ scripts.js
@@ -5,13 +5,31 @@ function newUserCheck() {
 		const email = document.getElementById("email").value;
 
 		if (username.length < 6) {
-				document.getElementById("usernameLabel").innerHTML = "<p style='color:red'>Username must be longer than 6 characters.</p>";
+				document.getElementById("usernameLabel").innerHTML = "Username <p style='color:red'>(Username must be longer than 6 characters.)</p>";
+		}
+		else {
+				document.getElementById("usernameLabel").innerHTML = "Username";
+		}
+
+		if (password.length < 6) {
+				document.getElementById("passwordLabel").innerHTML = "Password <p style='color:red'>(Password must be longer than 6 characters.)</p>";
+		}
+		else {
+				document.getElementById("passwordLabel").innerHTML = "Password";
+		}
+
+		emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		if (email.match(emailRegex)) {
+				document.getElementById("emailLabel").innerHTML = "Email <p style='color:red'>(Please use a valid email.)</p>";
+		}
+		else {
+				document.getElementById("emailLabel").innerHTML = "Email";
 		}
 }
 
-document.getElementById("submit-btn").addEventListener("click",function(event) {
+document.getElementById("new-user-submit").addEventListener("click",function(event) {
 		let test = newUserCheck();
 		if (true) {
 				event.preventDefault()
 		}
-}
+});
