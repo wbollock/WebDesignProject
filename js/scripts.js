bd4feb9e884f384c0dbec4af38ce8698f0c8c12b
Isaac Wolfe <iwwolfe97@gmail.com>
2019-04-22 18:08:23 -0400

    Commented out email regex until it works

--- js/scripts.js
+++ js/scripts.js
@@ -26,13 +26,13 @@ function newUserCheck() {
 				document.getElementById("teamnameLabel").innerHTML = "Team Name <p style='color:red'>(Team Name must not be empty.)</p>";
 		}
 
-		emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-		if (!email.match(emailRegex)) {
-				document.getElementById("emailLabel").innerHTML = "Email <p style='color:red'>(Please use a valid email.)</p>";
-		}
-		else {
-				document.getElementById("emailLabel").innerHTML = "Email";
-		}
+		// emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		// if (!email.match(emailRegex)) {
+		// 		document.getElementById("emailLabel").innerHTML = "Email <p style='color:red'>(Please use a valid email.)</p>";
+		// }
+		// else {
+		// 		document.getElementById("emailLabel").innerHTML = "Email";
+		// }
 }
 
 document.getElementById("submit-btn").addEventListener("click",function(event) {
