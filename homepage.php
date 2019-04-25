1c3c024c65fa5997001dad01c43080f1fee2449e
Isaac Wolfe <iwwolfe97@gmail.com>
2019-04-24 21:37:02 -0400

    Should fix extra 2 buttons that float

--- homepage.php
+++ homepage.php
@@ -12,7 +12,7 @@
 			<h1>Your Team Home Page</h1>
 			<?php
 			$username = $_GET["username"];
-			
+			echo "<p>Welcome $username from process.php</p>";
 		   
 			// copied from process.php
 			if ( !( $database = mysqli_connect( "dbsrv2.cs.fsu.edu","bollock","1hSObrpVvNhq")))
@@ -35,16 +35,16 @@
 				}
 
 			$team = mysqli_fetch_row($result);
-				echo "<p> Team ".$team[0]."</p>";
+				echo "<p> Your team is: ".$team[0]."</p>";
 			?>
 
 		   <!-- team table --> <!--switch to team name -->
 				<!--<caption> Your AAF team </caption>-->
 				<?php 
 				
-				
+				echo "<p> Echoing table </p>";
 				$queryPlayers = "SELECT * from Players where team_name='".$team[0]."';";
-				
+				echo "<p> query is: ".$queryPlayers;
 				echo "<table border='1'>";
 			  
 				
@@ -54,7 +54,7 @@
 					die( mysqli_error() );
 				}
 				// debug
-				
+				echo "<p> Executed query </p>";
 
 				while ($row = mysqli_fetch_array( $result ))
 				{
@@ -66,11 +66,11 @@
 				}
 				// debug
 				echo "</table>";
-				
+				echo "<p> Fetched rows </p>";
 				
 				?>
 			<?php
-		echo "<h2><a class=\"btn\" href=http://ww2.cs.fsu.edu/~bollock/addplayers.php?teamname=$team[0]&username=$username> Add Players to your Team</h2>";
+		echo "<h2><a class=\"btn\" href=http://ww2.cs.fsu.edu/~bollock/addplayers.php?teamname=$team[0]&username=$username>Add Players to your Team</a></h2>";
 		?>
 		</div>
 </body>
